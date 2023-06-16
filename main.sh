24a90c50f9d03d59ce9036594f30e786f0362de6
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:27:05 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,5 @@
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-
 # Clone Upstream
 git clone https://github.com/KyleGospo/jupiter-fan-control
 git clone https://github.com/KyleGospo/jupiter-hw-support
@@ -17,7 +11,6 @@ cp -rvf ./debian ./steamdeck-jupiter-driver
 cd ./steamdeck-jupiter-driver
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 apt-get build-dep ./ -y
 
 # Build package
